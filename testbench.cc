392252cb0b7fd74a94d2d2d8ce9549e644e8bc63
Ahmet Inan <xdsopl@gmail.com>
2026-06-03 14:51:55 +0200

    made expensive loop optional

--- testbench.cc
+++ testbench.cc
@@ -17,13 +17,16 @@ int main(int argc, char **argv)
 	(void)argc; (void)argv;
 	CM31 G(M31(2), M31(1268011823));
 	assert(norm(G) == M31(1));
-	assert(pow(G, 1U << 30) == -CM31(M31(1)));
-	assert(pow(G, 1U << 31) == CM31(M31(1)));
-	CM31 tmp(G);
-	for (uint32_t i = 1; i < M31::P+1; ++i)
-		assert(norm(tmp *= G) == M31(1) && tmp != G);
-	tmp *= G;
-	assert(tmp == G);
+	uint32_t order = M31::P + 1;
+	assert(pow(G, order / 2) == -CM31(M31(1)));
+	assert(pow(G, order) == CM31(M31(1)));
+	if (0) {
+		CM31 tmp(G);
+		for (uint32_t i = 1; i < order; ++i)
+			assert(norm(tmp *= G) == M31(1) && tmp != G);
+		tmp *= G;
+		assert(tmp == G);
+	}
 	return 0;
 }
 
