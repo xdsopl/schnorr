05f88b27709894079af044ddf2f1bdf205209bae
Ahmet Inan <xdsopl@gmail.com>
2026-06-04 07:14:46 +0200

    added hash function

--- testbench.cc
+++ testbench.cc
@@ -16,25 +16,25 @@ typedef CODE::ComplexField<M31> CM31;
 static const CM31 generator(M31(2), M31(1268011823));
 static const uint32_t order = M31::P + 1;
 
-void sign(uint32_t private_key, uint32_t nonce, uint32_t *scalar, uint32_t *check, const uint8_t *message, int length)
+uint32_t hash(CM31 point, const uint8_t *message, int length)
 {
-	CM31 point = pow(generator, nonce);
 	uint32_t hash = point.real()() ^ point.imag()();
 	for (int i = 0; i < length; ++i)
 		hash ^= message[i];
-	hash %= order;
-	*check = hash;
-	*scalar = (nonce + (uint64_t(hash) * private_key)) % order;
+	return hash % order;
+}
+
+void sign(uint32_t private_key, uint32_t nonce, uint32_t *scalar, uint32_t *check, const uint8_t *message, int length)
+{
+	CM31 point = pow(generator, nonce);
+	*check = hash(point, message, length);
+	*scalar = (nonce + (uint64_t(*check) * private_key)) % order;
 }
 
 bool verify(CM31 fingerprint, uint32_t scalar, uint32_t check, const uint8_t *message, int length)
 {
 	CM31 point = pow(generator, scalar) * pow(fingerprint, check);
-	uint32_t hash = point.real()() ^ point.imag()();
-	for (int i = 0; i < length; ++i)
-		hash ^= message[i];
-	hash %= order;
-	return hash == check;
+	return check == hash(point, message, length);
 }
 
 int main(int argc, char **argv)
