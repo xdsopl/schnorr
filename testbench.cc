409a14fcacd59f91004687e80373f8ffcfa5469f
Ahmet Inan <xdsopl@gmail.com>
2026-06-08 10:40:23 +0200

    refactored BSGS to own function

--- testbench.cc
+++ testbench.cc
@@ -62,6 +62,60 @@ bool verify(EC fingerprint, uint32_t scalar, uint32_t check, const uint8_t *mess
 	return check == hash(point, message, length);
 }
 
+int64_t bsgs(EC base, EC target, int64_t bound)
+{
+	const int m = (int)std::ceil(std::sqrt((double)bound));
+	assert(m <= 65536);
+	uint64_t *bs_tmp = new uint64_t[m];
+	EC baby;
+	for (int i = 0; i < m; ++i) {
+		bs_tmp[i] = uint64_t(i) << 32 | baby.compress();
+		baby += base;
+	}
+	EC giant = -baby;
+	auto index = [](uint32_t val) { return ((val >> 16) ^ val) & 65535; };
+	uint8_t *bs_cnt = new uint8_t[65536];
+	for (int i = 0; i < 65536; ++i)
+		bs_cnt[i] = 0;
+	for (int i = 0; i < m; ++i)
+		bs_cnt[index(bs_tmp[i] & 0xFFFFFFFF)]++;
+	uint16_t *bs_ptr = new uint16_t[65536];
+	bs_ptr[0] = 0;
+	for (int i = 1; i < 65536; ++i)
+		bs_ptr[i] = bs_ptr[i - 1] + bs_cnt[i - 1];
+	for (int i = 0; i < 65536; ++i)
+		bs_cnt[i] = 0;
+	uint64_t *bs_val = new uint64_t[m];
+	for (int i = 0; i < m; ++i) {
+		int idx = index(bs_tmp[i] & 0xFFFFFFFF);
+		bs_val[bs_ptr[idx] + bs_cnt[idx]++] = bs_tmp[i];
+	}
+	delete[] bs_tmp;
+	EC gamma(target);
+	for (int i = 0; i < m; ++i) {
+		uint32_t val = gamma.compress();
+		int idx = index(val);
+		int ptr = bs_ptr[idx];
+		int cnt = bs_cnt[idx];
+		for (int j = ptr; j < ptr + cnt; ++j) {
+			if (uint32_t(bs_val[j] & 0xFFFFFFFF) == val) {
+				uint32_t num = (bs_val[j] >> 32) & 65535;
+				delete[] bs_val;
+				delete[] bs_cnt;
+				delete[] bs_ptr;
+				std::cerr << "found target after " << m << " baby and " << i << " giant steps" << std::endl;
+				return int64_t(i) * m + num;
+			}
+		}
+		gamma += giant;
+	}
+	std::cerr << "what?" << std::endl;
+	delete[] bs_val;
+	delete[] bs_cnt;
+	delete[] bs_ptr;
+	return -1;
+}
+
 int main(int argc, char **argv)
 {
 	(void)argc; (void)argv;
@@ -138,55 +192,9 @@ int main(int argc, char **argv)
 	}
 	// BSGS
 	if (0) {
-		const int m = (int)std::ceil(std::sqrt((double)order));
-		assert(m <= 65536);
-		uint64_t *bs_tmp = new uint64_t[m];
-		EC baby;
-		for (int i = 0; i < m; ++i) {
-			bs_tmp[i] = uint64_t(i) << 32 | baby.compress();
-			baby += base;
-		}
-		EC giant = -baby;
-		auto index = [](uint32_t val) { return ((val >> 16) ^ val) & 65535; };
-		uint8_t *bs_cnt = new uint8_t[65536];
-		for (int i = 0; i < 65536; ++i)
-			bs_cnt[i] = 0;
-		for (int i = 0; i < m; ++i)
-			bs_cnt[index(bs_tmp[i] & 0xFFFFFFFF)]++;
-		uint16_t *bs_ptr = new uint16_t[65536];
-		bs_ptr[0] = 0;
-		for (int i = 1; i < 65536; ++i)
-			bs_ptr[i] = bs_ptr[i - 1] + bs_cnt[i - 1];
-		for (int i = 0; i < 65536; ++i)
-			bs_cnt[i] = 0;
-		uint64_t *bs_val = new uint64_t[m];
-		for (int i = 0; i < m; ++i) {
-			int idx = index(bs_tmp[i] & 0xFFFFFFFF);
-			bs_val[bs_ptr[idx] + bs_cnt[idx]++] = bs_tmp[i];
-		}
-		delete[] bs_tmp;
-		EC gamma(-public_key);
-		for (int i = 0; i < m; ++i) {
-			uint32_t val = gamma.compress();
-			int idx = index(val);
-			int ptr = bs_ptr[idx];
-			int cnt = bs_cnt[idx];
-			for (int j = ptr; j < ptr + cnt; ++j) {
-				if (uint32_t(bs_val[j] & 0xFFFFFFFF) == val) {
-					uint32_t num = (bs_val[j] >> 32) & 65535;
-					uint32_t key = (uint64_t(i) * m + num) % order;
-					assert(private_key == key);
-					std::cerr << "found private key after " << m << " baby and " << i << " giant steps" << std::endl;
-					goto end;
-				}
-			}
-			gamma += giant;
-		}
-		std::cerr << "what?" << std::endl;
-end:
-		delete[] bs_val;
-		delete[] bs_cnt;
-		delete[] bs_ptr;
+		uint64_t key = bsgs(base, -public_key, order);
+		assert(key >= 2 && key < order);
+		assert(private_key == uint32_t(key));
 	}
 	return 0;
 }
